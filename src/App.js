9dccd6a9df0db69fec42da7bbf677242d52b2a2a
Dhruv <dhruvg@iitbhilai.ac.in>
2024-09-29 07:30:02 +0530

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
+  const [tickets, setTickets] = useState([]);
+  const [users, setUsers] = useState([]);
+  const defaultGroup = localStorage.getItem('selectedGroup');
+  const defaultOrder = localStorage.getItem('selectedOrder');
+
+  const [group, setGroup] = useState(defaultGroup ? defaultGroup : 'status');
+  const [order, setOrder] = useState(defaultOrder ? defaultOrder : 'priority');
+
+
+  const handleGroupChange = (groupSelected) => {
+    setGroup(groupSelected);
+    localStorage.setItem("selectedGroup", groupSelected);
+  }
+  const handleOrderChange = (orderSelected) => {
+    setOrder(orderSelected);
+    localStorage.setItem("selectedOrder", orderSelected);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch('https://api.quicksell.co/v1/internal/frontend-assignment');
+      const data = await res.json();
+      setTickets(data.tickets);
+      setUsers(data.users);
+    } catch (error) {
+      console.log("Unable to fetch data! ", error);
+    }
+  }
+
   return (
-    <div className="App">
-      Kanban Board
+    <div className="App scroll-container">
+      <Navbar group={group} order={order} onGroupchange={handleGroupChange} onOrderChange={handleOrderChange} />
     </div>
   );
 }
